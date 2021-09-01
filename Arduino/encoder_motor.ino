dc1ab9ee8352633c360ad6a01dd2d88be53d96b2
PY <pyhuang97@gamil.com>
2021-09-02 02:53:05 +0800

    define all servo motors' working range

--- Arduino/encoder_motor.ino
+++ Arduino/encoder_motor.ino
@@ -25,47 +25,53 @@ AF_DCMotor right_motor(2, MOTOR12_8KHZ);
 class ServoMotor {
   private:
     const int channel;
-    volatile int target_angle;
-    volatile int curr_angle;
+    volatile int target_pulselen;
+    volatile int curr_pulselen;
+    const int min_angle;
+    const int max_angle;
     const int min_pulselen;
     const int max_pulselen;
+    const int rotate_speed;
     Adafruit_PWMServoDriver *pwm;
   
   public:
-    ServoMotor(Adafruit_PWMServoDriver*, int, int, int, int);
+    ServoMotor(Adafruit_PWMServoDriver*, int, int, int, int, int, int, int);
     int set_angle(int);
     int angle_to_pulselen(int);
-    void update_angle();
+    void update();
 };
 
-ServoMotor::ServoMotor(Adafruit_PWMServoDriver *pwm, int channel, int start_angle, const int min_pulselen, const int max_pulselen): channel(channel), min_pulselen(min_pulselen), max_pulselen(max_pulselen) {
+ServoMotor::ServoMotor(Adafruit_PWMServoDriver *pwm, int channel, int start_angle, const int rotate_speed, const int min_angle, const int max_angle, const int min_pulselen, const int max_pulselen): channel(channel), rotate_speed(rotate_speed), min_angle(min_angle), max_angle(max_angle), min_pulselen(min_pulselen), max_pulselen(max_pulselen) {
   this->pwm = pwm;
-  this->curr_angle = start_angle;
-  this->target_angle = start_angle;
+  this->curr_pulselen = this->angle_to_pulselen(start_angle);
+  this->target_pulselen = this->curr_pulselen;
 
-  pwm->setPWM(this->channel, 0, this->curr_angle);
+  delay(100);
+  pwm->setPWM(this->channel, 0, this->curr_pulselen);
+  delay(1000);
 }
 
 int ServoMotor::angle_to_pulselen(int angle) {
-  return map(angle, 0, 180, this->min_pulselen, this->max_pulselen);
+  
+  return map(angle, this->min_angle, this->max_angle, this->min_pulselen, this->max_pulselen);
 }
 
 int ServoMotor::set_angle(int angle) {
   
-  this->target_angle = angle;
+  this->target_pulselen = this->angle_to_pulselen(angle);
 }
 
-void ServoMotor::update_angle() {
+void ServoMotor::update() {
   
-  if(this->target_angle > this->curr_angle) {
-    ++this->curr_angle;
-    this->pwm->setPWM(this->channel, 0, curr_angle);
-  } else if (this->target_angle < this->curr_angle) {
-    --this->curr_angle;
-    this->pwm->setPWM(this->channel, 0, curr_angle);
+  if(this->target_pulselen > this->curr_pulselen) {
+    this->curr_pulselen += this->rotate_speed;
+    this->pwm->setPWM(this->channel, 0, curr_pulselen);
+  } else if (this->target_pulselen < this->curr_pulselen) {
+    this->curr_pulselen -= this->rotate_speed;
+    this->pwm->setPWM(this->channel, 0, curr_pulselen);
   } 
   
-  delay(10);
+  delay(5);
 }
 
 
@@ -77,7 +83,7 @@ class Arm {
                *arm_vertical1,
                *arm_vertical2,
                *arm_vertical3,
-               *gripper_rotate,
+               *gripper_rotation,
                *gripper,
                *webcam_horizontal,
                *webcam_vertical;
@@ -86,22 +92,30 @@ class Arm {
     void rotate_arm(int);
     void update_all();
     void move_arm_vertical1(int);
+    void move_arm_vertical2(int);
+    void move_arm_vertical3(int);
+    void rotate_gripper(int);
+    void move_gripper(int);
+    void move_webcam_vertical(int);
+    void move_webcam_horizontal(int);
 };
 
 Arm::Arm(int SERVO_FREQ) {
+  delay(100);
   this->pwm = Adafruit_PWMServoDriver();
   this->pwm.begin();
   this->pwm.setOscillatorFrequency(27000000);
   this->pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  delay(100);
 
-  this->base_horizontal = &ServoMotor(&this->pwm, 0, 150, 110, 460);
-  this->arm_vertical1 = &ServoMotor(&this->pwm, 1, 200, 180, 400);
-//  this->arm_vertical2 = &ServoMotor(&this->pwm, 0, 90, 110, 460);
-//  this->arm_vertical3 = &ServoMotor(&this->pwm, 0, 90, 110, 460);
-//  this->gripper_rotate = &ServoMotor(&this->pwm, 0, 90, 110, 460);
-//  this->gripper = &ServoMotor(&this->pwm, 0, 90, 110, 460);
-//  this->webcam_horizontal = &ServoMotor(&this->pwm, 0, 90, 110, 460);
-//  this->webcam_vertical = &ServoMotor(&this->pwm, 0, 90, 110, 460);
+  this->base_horizontal = new ServoMotor(&this->pwm, 0, 100, 5, 0, 180, 110, 460);
+  this->arm_vertical1 = new ServoMotor(&this->pwm, 1, 0, 1, 0, 180, 180, 400);
+  this->arm_vertical2 = new ServoMotor(&this->pwm, 2, 100, 1, 0, 180, 100, 460);
+  this->arm_vertical3 = new ServoMotor(&this->pwm, 3, 30, 1, 0, 180, 170, 410);
+  this->gripper_rotation = new ServoMotor(&this->pwm, 4, 27, 3, 0, 180, 80, 465);
+  this->gripper = new ServoMotor(&this->pwm, 5, 20, 5, 0, 90, 285, 420);
+  this->webcam_horizontal = new ServoMotor(&this->pwm, 6, 105, 5, 0, 180, 90, 470);
+  this->webcam_vertical = new ServoMotor(&this->pwm, 7, 60, 5, 0, 180, 90, 470);
 }
 
 void Arm::rotate_arm(int angle) {
@@ -112,9 +126,39 @@ void Arm::move_arm_vertical1(int angle) {
   this->arm_vertical1->set_angle(angle);
 }
 
+void Arm::move_arm_vertical2(int angle) {
+  this->arm_vertical2->set_angle(angle);
+}
+
+void Arm::move_arm_vertical3(int angle) {
+  this->arm_vertical3->set_angle(angle);
+}
+
+void Arm::rotate_gripper(int angle) {
+  this->gripper_rotation->set_angle(angle);
+}
+
+void Arm::move_gripper(int angle) {
+  this->gripper->set_angle(angle);
+}
+
+void Arm::move_webcam_horizontal(int angle) {
+  this->webcam_horizontal->set_angle(angle);
+}
+
+void Arm::move_webcam_vertical(int angle) {
+  this->webcam_vertical->set_angle(angle);
+}
+
 void Arm::update_all() {
-  this->base_horizontal->update_angle();
-  this->arm_vertical1->update_angle();
+  this->base_horizontal->update();
+  this->arm_vertical1->update();
+  this->arm_vertical2->update();
+  this->arm_vertical3->update();
+  this->gripper_rotation->update();
+  this->gripper->update();
+  this->webcam_horizontal->update();
+  this->webcam_vertical->update();
 }
 
 
@@ -129,6 +173,7 @@ class MyRobot {
     const EncoderMotor &right_wheel;
     int prev_diff = 0;
     int prev_t = 0;
+  
   public:
     MyRobot(const EncoderMotor&, const EncoderMotor&);
     void go(int, int);
@@ -254,26 +299,8 @@ void setup() {
   digitalWrite(Car_LED_Pin, HIGH);
   digitalWrite(Arm_LED_Pin, HIGH);
 
-  delay(10);
+  delay(100);
   arm = new Arm(50);
-  delay(10);
-
-////  Arm arm(0, 90, 110, 460, 50);
-//  delay(1000);
-//  arm.rotate_arm(400);
-//  for(int i=0; i<100; ++i) {
-//    arm.update_all();
-//  }
-//  delay(1000);
-//  
-
-//  ServoMotor servo_motor1(0, 90, 110, 460, 50);
-//  ServoMotor servo_motor2(0, 90, 110, 460, 50);
-//  ServoMotor servo_motor3(0, 90, 110, 460, 50);
-//  ServoMotor servo_motor4(0, 90, 110, 460, 50);
-//  ServoMotor servo_motor5(0, 90, 110, 460, 50);
-//  ServoMotor servo_motor6(0, 90, 110, 460, 50);
-//  ServoMotor servo_motor7(0, 90, 110, 460, 50);
 }
 
 char *command;
@@ -327,10 +354,31 @@ void loop() {
     else digitalWrite(Car_LED_Pin, HIGH);
     command = "STP";
   } else if(!strcmp(command, "SER")) {
-    if(value1 == 0) {
+    switch(value1) {
+    case 0:
       arm->rotate_arm(value2);  
-    } else if(value1 == 1) {
+      break;
+    case 1:
       arm->move_arm_vertical1(value2);
+      break;
+    case 2:
+      arm->move_arm_vertical2(value2);
+      break;
+    case 3:
+      arm->move_arm_vertical3(value2);
+      break;
+    case 4:
+      arm->rotate_gripper(value2);
+      break;
+    case 5:
+      arm->move_gripper(value2);
+      break;
+    case 6:
+      arm->move_webcam_horizontal(value2);
+      break;
+    case 7:
+      arm->move_webcam_vertical(value2);
+      break;
     }
     
     command = "STP";
@@ -340,7 +388,7 @@ void loop() {
 
   arm->update_all();
   
-  delay(10);
+  delay(5);
 }
 
 void Left_DC_Motor_Encoder() {
